c6d7c88227e681f4d5da5c5a2cde8628a806330a
unknown <frkaresz123@gmail.com>
2015-09-27 21:35:53 +0200

    algoritmus

--- hanoi.cpp
+++ hanoi.cpp
@@ -6,6 +6,7 @@ using namespace std;
 // n - pocet diskov kt. chceme presunut (napr. 3) z veze "pociatok" do veze "ciel"
 void hanoiske_veze(int n, int pociatok, int ciel, int odkladacia)
 {
+  // Algoritmus funkcie hanojske veze s rekurziou
   // Presunujem posledny disk (n = 0) z veze "pociatok " do veze "ciel" a vypisem o tom spravu
   if(n == 0) std::cout << "Presunieme kotuc "  << n << " z veze " << pociatok << " do veze " << ciel << endl;
 
